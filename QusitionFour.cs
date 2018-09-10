0a8005ca99ba799f56223ef4d1162c3f670079a0
Rinne <hsmh00@163.com>
2018-09-10 23:15:19 +0800

    homework-2018-09-10

---
+++ QusitionFour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace homework1
+{
+    class QustionFour
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input the first number:");
+            string FirstStr = Console.ReadLine();
+            float NumberOne = float.Parse(FirstStr);
+            Console.WriteLine("Please input the second number:");
+            string SecondStr = Console.ReadLine();
+            float NumberTwo = float.Parse(SecondStr);
+            Console.WriteLine("The first number times the second number is:" + (NumberOne * NumberTwo));
+        }
+    }
+}
